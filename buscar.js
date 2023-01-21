0ada8f396e085b22023db68222ccd7299b78f151
farakservicos <123268376+farakservicos@users.noreply.github.com>
2023-01-21 19:39:02 -0300

    Update buscar.js

--- buscar.js
+++ buscar.js
@@ -2,7 +2,7 @@ function buscar(){
     var cod = window.document.getElementById('cod')
     var resul = window.document.getElementById('resul')
     if(cod.value =='285BA' || cod.value =='285ba'){
-        resul.innerHTML += '<br> <a class="btn btn-primary" href="bem-vindo.html">Entrar</a>'
+        resul.innerHTML = '<br> <a class="btn btn-primary" href="bem-vindo.html">Entrar</a>'
     }else{
         alert('Usuário não encontrado! ')
     }
